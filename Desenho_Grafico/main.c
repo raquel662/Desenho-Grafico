2bfc68aeb4a8045721a7ef74834dac4e15873f5c
raquel <raquel@raquel-Aspire-F5-573G>
2019-11-07 15:56:15 -0300

    Modificação - gerar arquivo ppm

--- Desenho_Grafico/main.c
+++ Desenho_Grafico/main.c
@@ -5,4 +5,36 @@
  *      Author: raquel
  */
 
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Imagem
+{
+	char tipo[2];
+	int dimencao[100][100];
+	int valor_maximoP;
+
+};
+
+
+/* code */
+int main(int argc, char const *argv[])
+{
+	/* criar ponteiro que aponta em tipos de data file */
+	FILE *imagem;
+
+    /* code */
+	imagem = fopen("imagem.ppm","r");
+
+    /* Testa se a abertura do arquivo deu certo
+    fopen retorna ponteiro pro arquivo em sucesso
+    e nulo caso o contrario */
+	if (imagem == NULL)
+	{
+		printf("Erro na abertura!!\n");
+		exit(1);
+	}
+
+	return 0;
+}
 
